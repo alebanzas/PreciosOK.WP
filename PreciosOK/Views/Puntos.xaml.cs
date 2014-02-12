312aac0cac28e1eda1b99363d36817944b269c26
ale <ale@localhost>
2014-02-12 10:54:18 -0300

    lista con ads

--- PreciosOK/Views/Puntos.xaml.cs
+++ PreciosOK/Views/Puntos.xaml.cs
@@ -91,7 +91,9 @@ namespace PreciosOK.Views
             {
                 ViewModel.AddEstacion(product);
             }
-            List.ScrollIntoView(List.Items.First());
+
+            ScrollViewer sv = List.Descendents().OfType<ScrollViewer>().FirstOrDefault();
+            if (sv != null) sv.ScrollToVerticalOffset(0);
         }
         
         private void LstLugares_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -167,6 +169,39 @@ namespace PreciosOK.Views
         private void CodeSearch_Click(object sender, EventArgs e)
         {
             NavigationService.Navigate(new Uri("/Views/Scan.xaml", UriKind.Relative));
+        }
+    }
+
+    public static class VisualTreeEnumeration
+    {
+        public static IEnumerable<DependencyObject> Descendents(this DependencyObject root, int depth)
+        {
+            int count = VisualTreeHelper.GetChildrenCount(root);
+            for (int i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(root, i);
+                yield return child;
+                if (depth > 0)
+                {
+                    foreach (var descendent in Descendents(child, --depth))
+                        yield return descendent;
+                }
+            }
+        }
+
+        public static IEnumerable<DependencyObject> Descendents(this DependencyObject root)
+        {
+            return Descendents(root, Int32.MaxValue);
+        }
+
+        public static IEnumerable<DependencyObject> Ancestors(this DependencyObject root)
+        {
+            DependencyObject current = VisualTreeHelper.GetParent(root);
+            while (current != null)
+            {
+                yield return current;
+                current = VisualTreeHelper.GetParent(current);
+            }
         }
     }
 
