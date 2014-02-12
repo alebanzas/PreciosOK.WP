35ec9d3c2227698ab841d276c19a03bc3f551d40
ale <ale@localhost>
2014-02-12 14:48:10 -0300

    cambio de version

--- PreciosOK/App.xaml.cs
+++ PreciosOK/App.xaml.cs
@@ -9,7 +9,7 @@ namespace PreciosOK
     public partial class App : Application
     {
         private const string AppName = "PreciosOKWP";
-        private const string AppVersion = "1.0.0.0";
+        private const string AppVersion = "1.0.0.1";
 
         public static ApplicationConfiguration Configuration { get; set; }
 
